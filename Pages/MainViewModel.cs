8cbd50a2ba3cf134beba6b39151afa50b87cc0df
Yaronzz <392309221@qq.com>
2020-05-30 17:39:49 +0800

    基本UI设计

---
+++ Pages/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using Stylet;
+
+namespace Votrix.Pages
+{
+    public class MainViewModel : Screen
+    {
+        public void WindowClose()
+        {
+            RequestClose();
+        }
+
+        public void WindowMin()
+        {
+            ((MainView)this.View).WindowState = WindowState.Minimized;
+        }
+
+        public void WindowMax()
+        {
+            if(((MainView)this.View).WindowState != WindowState.Maximized)
+                ((MainView)this.View).WindowState = WindowState.Maximized;
+            else
+                ((MainView)this.View).WindowState = WindowState.Normal;
+        }
+
+        public void WindowMove()
+        {
+            ((MainView)this.View).DragMove();
+        }
+    }
+}
